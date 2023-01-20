1786d5c6eb361d65e57e7d97c94ed7b7b770a2ea
AleksandrEpishkin <aepishkin@yandex.ru>
2023-01-20 14:36:24 +0300

    Intent test

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/IntentTest.java
@@ -0,0 +1,60 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import android.content.Intent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.Matchers;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+
+@LargeTest
+@RunWith(AllureAndroidJUnit4.class)
+
+public class IntentTest {
+
+    @Rule
+//    public ActivityTestRule<MainActivity> mActivityScenarioRule =
+//            new ActivityTestRule<>(MainActivity.class);
+    public IntentsTestRule intentsTestRule = new IntentsTestRule(MainActivity.class);
+
+    @Test
+    public void intentTest() {
+        ViewInteraction imageView = onView(
+                Matchers.allOf(
+                        withParent(withParent(withId(R.id.toolbar)))));
+        imageView.check(matches(isDisplayed()));
+        imageView.perform(click());
+
+
+        ViewInteraction materialTextView = onView(
+                allOf(withId(androidx.constraintlayout.widget.R.id.title), withText("Settings"),
+                        isDisplayed()));
+
+//        Intents.init();
+        materialTextView.perform(click());
+        intended(allOf(
+            hasData("https://google.com"),
+            hasAction(Intent.ACTION_VIEW)
+        ));
+//        Intents.release();
+
+    }
+}
